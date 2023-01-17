941d2252440ca0c3c740c9df96463f36ef5f9717
Asraful Islam <iasraful321@gmail.com>
2023-01-17 23:56:15 +0600

    added Product section__ Needs some changes

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -3,6 +3,20 @@
 import React from "react";
 
 function Product() {
-  return <div>Product</div>;
+  const images = [
+    "https://images.pexels.com/photos/9324653/pexels-photo-9324653.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    "https://images.pexels.com/photos/14318107/pexels-photo-14318107.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  ];
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="Men" />
+          <img src={images[1]} alt="Women" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
 export default Product;
