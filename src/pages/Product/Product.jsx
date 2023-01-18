100e91edca1561194eed4d42056f4d825be9fc9c
Asraful Islam <iasraful321@gmail.com>
2023-01-19 01:31:57 +0600

    added product section

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,18 +1,24 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+import "./Product.scss";
 
 function Product() {
+  const [selectedImg, setSelectedImg] = useState(0);
   const images = [
     "https://images.pexels.com/photos/9324653/pexels-photo-9324653.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     "https://images.pexels.com/photos/14318107/pexels-photo-14318107.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
   ];
+
   return (
     <div className="product">
       <div className="left">
         <div className="images">
-          <img src={images[0]} alt="Men" />
-          <img src={images[1]} alt="Women" />
+          <img src={images[0]} alt="Men" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="Women" onClick={(e) => setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
         </div>
       </div>
       <div className="right"></div>
